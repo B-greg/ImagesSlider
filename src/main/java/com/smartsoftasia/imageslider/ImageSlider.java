d12966470b251bfd8f7d590f1f6d8a96ac669747
gregoire <greg@smartsoftasia.com>
2015-06-16 15:03:48 +0700

    Set design home page.

---
+++ src/main/java/com/smartsoftasia/imageslider/ImageSlider.java
@@ -0,0 +1,17 @@
+package com.smartsoftasia.imageslider;
+
+/**
+ * Created by gregoire barret on 3/13/15.
+ * For neibo-android project.
+ */
+public class ImageSlider {
+    public static final String TAG = "SliderImage";
+
+    public String url;
+    public String name;
+
+    public ImageSlider(String url, String name) {
+        this.url = url;
+        this.name = name;
+    }
+}
